54ee99573225e9620b3ad45c9ccd6ff633fba54f
Roman Grubic <grubic.roman@gmail.com>
2022-05-16 12:52:04 +0200

    Extracting language logic to validateLanguage() method.

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -20,21 +20,8 @@ class TestController extends Controller
 {
     public function index(Request $request)
     {
-//        Getting lang from GET
-        $lang = $request->input('lang');
-//        Checking if the GET lang exist in database
-//        Ugly, but I got what I needed :/
-        $codes = Languages::readCode();
-        $codeList = [];
-            foreach ($codes as $code) {
-                    $codeList[] = $code->code;
-            }
-//        Checking if $lang in array. If not, default to 'en' -> english
-        if (!in_array($lang, $codeList)) {
-            $lang = 'en';
-        }
-//        Setting locale to the $lang
-        App::setLocale($lang);
+//        Validate function for language, default 'en'
+        $this->validateLanguage($request->input('lang'));
 
 //        Getting parameters from Request GET only if they are not null
 //        "lang" is already set and "with" is not going to Meals
@@ -44,5 +31,22 @@ class TestController extends Controller
 
         return response()->json($data);
 
+    }
+
+    private function validateLanguage($language)
+    {
+//        Get all languages from DB to check if GET['lang'] exists or not!
+        $codes = Languages::readCode();
+//        Putting code values in array codeList to perform check
+        $codeList = [];
+        foreach ($codes as $code) {
+            $codeList[] = $code->code;
+        }
+//        Check if $lang in array. If not, default to 'en' -> English
+        if (!in_array($language, $codeList)) {
+            $language = 'en';
+        }
+//        Setting locale to the $lang
+        App::setLocale($language);
     }
 }
