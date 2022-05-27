16a548eb8d8d177546e0bd3ed485d3996c9d6d9d
Roman Grubic <grubic.roman@gmail.com>
2022-05-27 22:31:07 +0200

    Removing appendWith method and refactoring (big time)

--- app/Http/Controllers/MealsController.php
+++ app/Http/Controllers/MealsController.php
@@ -3,19 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\MealsGetRequest;
-use App\Models\Categories;
-use App\Models\CategoriesTranslation;
-use App\Models\Ingredients;
-use App\Models\IngredientsTranslation;
-use App\Models\Languages;
 use App\Models\Meals;
-use App\Models\MealsIngredients;
-use App\Models\MealsTags;
-use App\Models\Tags;
-use App\Models\TagsTranslation;
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\App;
+
 
 class MealsController extends Controller
 {
@@ -23,57 +13,40 @@ class MealsController extends Controller
 //    For dependency injection
     protected $meals;
     protected $request;
-    protected $languages;
-    protected $app;
-    protected $categories;
-    protected $categoriesTranslation;
-    protected $mealsTags;
-    protected $tags;
-    protected $tagsTranslation;
-    protected $mealsIngredients;
-    protected $ingredients;
-    protected $ingredientsTranslation;
 
     /**
      * Create a new controller instance.
      *
      * @param  Meals  $meals
+     * @param MealsGetRequest $request
      * @return void
      */
-    public function __construct(Meals $meals,
-                                MealsGetRequest $request,
-                                Categories $categories,
-                                CategoriesTranslation $categoriesTranslation,
-                                MealsTags $mealsTags,
-                                Tags $tags,
-                                TagsTranslation $tagsTranslation,
-                                MealsIngredients $mealsIngredients,
-                                Ingredients $ingredients,
-                                IngredientsTranslation $ingredientsTranslation)
+    public function __construct(Meals $meals, MealsGetRequest $request)
     {
         $this->meals = $meals;
         $this->request = $request;
-        $this->categories = $categories;
-        $this->categoriesTranslation = $categoriesTranslation;
-        $this->mealsTags = $mealsTags;
-        $this->tags = $tags;
-        $this->tagsTranslation = $tagsTranslation;
-        $this->mealsIngredients = $mealsIngredients;
-        $this->ingredients = $ingredients;
-        $this->ingredientsTranslation = $ingredientsTranslation;
     }
 
 //    Main method
     public function index(): JsonResponse
     {
-        $validated = $this->request->validated();
-
         $parameters = [];
-        foreach ($validated as $k => $v) {
+        foreach ($this->request->validated() as $k => $v) {
             $parameters += [$k => $v];
         }
 //        dd($parameters);
+        $array = $this->meals::readMeals($parameters);
+        $newList = [];
+//        dd($array);
+        foreach ($array['data'] as $o) {
+            foreach ($o as $k => $v) {
+                $newList[] = $v;
+            }
+        }
+//        dd($newList);
+        $data = $this->meals::with(['tags', 'ingredients', 'category'])->whereIn('id', $newList)->get()->toArray();
 
+        return response()->json($data);
 
 //        Getting data from the query
         $data = $this->meals::readMeals($parameters);
@@ -149,51 +122,5 @@ class MealsController extends Controller
         $data['links']['self'] .= '?page=' . $this->request->input('page') . $route;
 
         return $data;
-    }
-
-//    Takes with parameter and appends desired data
-    private function appendWith($with, $data)
-    {
-        $withList = explode(',', $with);
-//        If 'with' array contains 'category'
-        if (in_array('category', $withList)) {
-            foreach ($data['data'] as $meal) {
-                if ($meal->category_id != null) {
-                    $category = $this->categories::getCategory($meal->category_id);
-                    $object = $this->categoriesTranslation::getTitle($category->id);
-                    $category->title = $object->title;
-                    unset($meal->category_id);
-                    $meal->category = $category;
-                }
-            };
-        }
-//        If 'with' array contains 'tags'
-        if (in_array('tags', $withList)) {
-            foreach ($data['data'] as $meal) {
-                $tags = $this->mealsTags::searchByMealId($meal->id);
-                $tagList = [];
-                foreach ($tags as $tag) {
-                    $t = $this->tags::getTag($tag->tags_id);
-                    $object = $this->tagsTranslation::getTitle($t->id);
-                    $t->title = $object->title;
-                    $tagList[] = $t;
-                }
-                $meal->tags = $tagList;
-            };
-        }
-//        If 'with' array contains 'ingredients'
-        if (in_array('ingredients', $withList)) {
-            foreach ($data['data'] as $meal) {
-                $ingredients = $this->mealsIngredients::searchByMealId($meal->id);
-                $ingredientsList = [];
-                foreach ($ingredients as $ingredient) {
-                    $i = $this->ingredients::getIngredient($ingredient->ingredients_id);
-                    $object = $this->ingredientsTranslation::getTitle($i->id);
-                    $i->title = $object->title;
-                    $ingredientsList[] = $i;
-                }
-                $meal->ingredients = $ingredientsList;
-            };
-        }
     }
 }
