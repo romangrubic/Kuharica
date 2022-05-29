34eda35e2bc6fc9f80c9d0939b38b82fd6c10916
Roman Grubic <grubic.roman@gmail.com>
2022-05-29 14:09:56 +0200

    Major refactoring of Meals model.

--- app/Models/Meals.php
+++ app/Models/Meals.php
@@ -13,8 +13,7 @@ use Illuminate\Database\Eloquent\{Factories\HasFactory,
     Relations\HasMany,
     SoftDeletes,
     Relations\BelongsToMany};
-use Illuminate\Support\Facades\{App,
-    DB};
+use Illuminate\Support\Facades\App;
 
 /**
  * Meals is a model class for meals table.
@@ -54,77 +53,41 @@ class Meals extends Model
     {
         return $this->hasMany(MealsTranslation::class)->where('locale', '=', App::getLocale());
     }
-
-//    For future me: whole point of readMeals is to get list of Id-s so that I can pass it to MealController eloquent query
+    
     public static function readMeals($parameters): array
     {
-//        Main query
-        $meals = Meals::select('id')
-//            ->join('meals_tags', 'meals_tags.meals_id', '=', 'meals.id', 'inner')
-            ->where(function ($query) use ($parameters) {
-                if (!isset($parameters['diff_time'])) {
-                    $query->where('deleted_at', '=', null);
-                }
-            })
-            ->where(function ($query) use ($parameters) {
-                if (!isset($parameters['category'])) {
-                    return;
-                }
-                switch ($parameters['category']) {
-                    case('NULL'):
-                        $query->whereNull('category_id');
-                        break;
-                    case('!NULL'):
-                        $query->whereNotNull('category_id');
-                        break;
-                    default:
-                        $query->where('category_id', '=', $parameters['category']);
-                        break;
-                }
-            })
-            ->when(isset($parameters['tags']), function ($query) use ($parameters) {
-                $requestedTagIds = explode(',', $parameters['tags']);
-//                dd($parameters['tags']);
-                return $query->whereHas(
-                    'tags',
-                    fn($query) => $query->whereIn('tags_id', $requestedTagIds),
-                    '=',
-                    count($requestedTagIds)
-                );
-            })
-            ->where(function ($query) use ($parameters) {
-                if (isset($parameters['diff_time'])) {
+            return Meals::select('*')
+                ->when(isset($parameters['category']), function ($query) use ($parameters) {
+                    switch ($parameters['category']) {
+                        case('NULL'):
+                            $query->whereNull('category_id');
+                            break;
+                        case('!NULL'):
+                            $query->whereNotNull('category_id');
+                            break;
+                        default:
+                            $query->where('category_id', '=', $parameters['category']);
+                            break;
+                    }
+                })
+                ->when(isset($parameters['tags']), function ($query) use ($parameters) {
+                    $requestedTagIds = explode(',', $parameters['tags']);
+                    return $query->whereHas(
+                        'tags',
+                        fn($query) => $query->whereIn('tags_id', $requestedTagIds),
+                        '=',
+                        count($requestedTagIds)
+                    );
+                })
+                ->when(isset($parameters['with']), function ($query) use ($parameters) {
+                    $query->with(explode(',', $parameters['with']));
+                })
+                ->when(isset($parameters['diff_time']), function ($query) use ($parameters) {
                     $timestamp = Carbon::createFromTimestamp($parameters['diff_time']);
-                    $query->where('updated_at', '>=', $timestamp);
-                };
-            })
-            ->paginate(((isset($parameters['per_page'])) ? $parameters['per_page'] : 10), '[*]', 'page', ((isset($parameters['page'])) ? $parameters['page'] : 1))
-            ->toArray();
-
-        return $meals;
-//        Get translations and put them in their place
-        foreach ($meals['data'] as $meal) {
-            if (!isset($parameters['diff_time'])) {
-                $meal->status = 'created';
-            } else {
-//                dd($meal);
-                if ($meal->created_at == $meal->updated_at) {
-                    $meal->status = 'created';
-                } else {
-                    $meal->status = 'modified';
-                }
-                if ($meal->deleted_at != null) {
-                    $meal->status = 'deleted';
-                }
-            }
-            $translation = MealsTranslation::getTitleAndDescription($meal->id);
-            $meal->title = $translation->title;
-            $meal->description = $translation->description;
-        }
-
-//        Count for meta['totalItems'].
-        $countMeals = $meals['total'];
-
-        return [$meals, $countMeals];
+                    $query->where('meals.updated_at', '>=', $timestamp);
+                    $query->withTrashed();
+                    })
+                ->paginate($parameters['per_page'], '[*]', 'page', $parameters['page'])
+                ->toArray();
     }
 }
