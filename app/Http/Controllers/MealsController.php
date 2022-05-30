b6d1adc58812ae07dd58399f7744ddb4fd8ccbc1
Roman Grubic <grubic.roman@gmail.com>
2022-05-30 15:25:35 +0200

    Commenting MealsController

--- app/Http/Controllers/MealsController.php
+++ app/Http/Controllers/MealsController.php
@@ -14,11 +14,10 @@ use Illuminate\Http\JsonResponse;
 /**
  * MealsController is a controller class for Meals model.
  */
-
 class MealsController extends Controller
 {
     /**
-     * Properties for dependency injection and their types
+     * Properties for dependency injection and their types.
      */
     protected Meals $meals;
     protected MealsGetRequest $request;
@@ -26,7 +25,7 @@ class MealsController extends Controller
     /**
      * Create a new controller instance with dependencies.
      *
-     * @param  Meals  $meals
+     * @param Meals  $meals
      * @param MealsGetRequest $request
      * @return void
      */
@@ -37,27 +36,33 @@ class MealsController extends Controller
     }
 
     /**
-     * index() is main method for MealsController class
-     *
-     * Form request (MealsGetRequests.php) is validating the user input (GET parameters)
-     * Query (Meals::readMeals in Models\Meals.php) is sent with those parameters
-     * Result is passed to MealsCollection (MealsCollection.php and MealsResource.php) for formatting response
-     * Display a list of meals with requested parameters in Json for user.
+     * index() is main method for MealsController class.
      *
      * @return JsonResponse
      */
     public function index(): JsonResponse
     {
-        return response()->json(new MealsCollection($this->meals::readMeals($this->request->validated())));
+        /**
+         * Form request (MealsGetRequests.php) is validating the user input (GET parameters).
+         * Packing GET parameters in an array $parameters.
+         */
+        $parameters = $this->request->validated();
 
         /**
-         *  Code below does the sam job as above, but is more readable
+         * Query (Meals::readMeals in Models\Meals.php) is sent with those parameters.
+         */
+        $data = $this->meals::readMeals($parameters);
+
+        /**
+         * Result is passed to MealsCollection (MealsCollection.php and MealsResource.php) for formatting data.
+         * Display a list of meals with requested parameters in Json for user.
+         */
+        return response()->json(new MealsCollection($data));
+
+        /**
+         * Or in one line
          *
-         *  $parameters = $this->request->validated();
-         *
-         *  $data = $this->meals::readMeals($parameters);
-         *
-         *  return response()->json(new MealsCollection($data));
+         * return response()->json(new MealsCollection($this->meals::readMeals($this->request->validated())));
          */
     }
 }
