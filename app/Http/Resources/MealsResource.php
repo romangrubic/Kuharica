76b335dd171be6acfc5abdbb5c1bff3e30d65d39
Roman Grubic <grubic.roman@gmail.com>
2022-05-30 18:14:00 +0200

    Moving helper method from MealsResource to MealsResourceHelpers.

--- app/Http/Resources/MealsResource.php
+++ app/Http/Resources/MealsResource.php
@@ -1,15 +1,18 @@
 <?php
 
+/**
+ * This file contains formatting instructions for data (meals items).
+ */
 namespace App\Http\Resources;
 
 use App\Http\Requests\MealsGetRequest;
-use Illuminate\Http\Request;
+use App\Http\Resources\ResourcesHelpers\MealsResourceHelpers;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class MealsResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
+     * Transform the resource into an array with specified keys.
      *
      * @param  MealsGetRequest  $request
      * @return array
@@ -20,88 +23,17 @@ class MealsResource extends JsonResource
             'id' => $this->resource['id'],
             'title' => $this->resource['meals_translations'][0]['title'],
             'description' => $this->resource['meals_translations'][0]['description'],
-            'status' => $this->helperStatus($request['diff_time']),
-            'category' => $this->helperCategory($request['with']),
+            'status' => MealsResourceHelpers::helperStatus($request['diff_time'], $this->resource),
+            'category' => MealsResourceHelpers::helperCategory($request['with'], $this->resource),
         ];
 
         foreach (['tags', 'ingredients'] as $item) {
-            $data = $this->helperWithData($request, $item);
+            $data = MealsResourceHelpers::helperWithData($request, $item, $this->resource);
             if ($data) {
                 $meal[$item] = $data;
             }
         }
 
         return $meal;
-    }
-
-    /**
-     * Sets 'status' key with correct status type (created|modified|deleted)
-     *
-     * @param string $param
-     * @return string
-     */
-    private function helperStatus(string $param): string
-    {
-        if (!isset($param)) {
-            return 'created';
-        }
-
-        if ($this->resource['deleted_at'] != null) {
-            return 'deleted';
-        }
-
-        if ($this->resource['created_at'] == $this->resource['updated_at']) {
-            return 'created';
-        } else {
-            return 'modified';
-        }
-    }
-
-    /**
-     * Sets 'category' key with category data
-     *
-     * @param string $param
-     * @return array|mixed
-     * if I write type array|mixed next to function, it throws a syntax error because of |
-     */
-    private function helperCategory(string $param)
-    {
-        if (!in_array('category', explode(',', $param))) {
-            return $this->resource['category_id'];
-        }
-
-        if ($this->resource['category'] == null) {
-            return $this->resource['category_id'];
-        }
-
-        return [
-            'id' => $this->resource['category']['id'],
-            'title' => $this->resource['category']->toArray()['categories_translations'][0]['title'],
-            'slug' => $this->resource['category']['slug']
-        ];
-    }
-
-    /**
-     * Sets keys 'tags' and 'ingredients' if they are set in GET 'with' parameter
-     *
-     * @param Request $request
-     * @param string $key
-     * @return array|bool
-     * if I write type array|mixed next to function, it throws a syntax error because of |
-     */
-    private function helperWithData(Request $request, string $key)
-    {
-        if (in_array($key, explode(',', $request->input('with')))) {
-            $dataList = [];
-            foreach ($this->resource[$key] as $item) {
-                $data['id'] = $item->id;
-                $data['title'] = $item->toArray()[$key . '_translations'][0]['title'];
-                $data['slug'] = $item->slug;
-
-                $dataList[] = $data;
-            }
-            return $dataList;
-        };
-        return false;
     }
 }
---
+++ app/Http/Resources/ResourcesHelpers/MealsResourceHelpers.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * This file contains helper methods for MealsResource.
+ * Keeping them in MealsResource would violate SOLID (to my understanding of it)
+ * so I made a MealsResourceHelpers class for them.
+ */
+
+namespace App\Http\Resources\ResourcesHelpers;
+
+use App\Models\Meals;
+use Illuminate\Http\Request;
+
+/**
+ * MealsResourceHelpers class contains helper methods for MealsResource.
+ */
+class MealsResourceHelpers
+{
+    /**
+     * Sets 'status' key with correct status type (created|modified|deleted)
+     *
+     * @param string $param
+     * @param Meals $resource
+     * @return string
+     */
+    public static function helperStatus(string $param, Meals $resource): string
+    {
+        if (!isset($param)) {
+            return 'created';
+        }
+
+        if ($resource['deleted_at'] != null) {
+            return 'deleted';
+        }
+
+        if ($resource['created_at'] == $resource['updated_at']) {
+            return 'created';
+        } else {
+            return 'modified';
+        }
+    }
+
+    /**
+     * Sets 'category' key with category data
+     *
+     * @param string $param
+     * @param Meals $resource
+     * @return array|mixed
+     * if I write type array|mixed next to function, it throws a syntax error because of |
+     */
+    public static function helperCategory(string $param, Meals $resource)
+    {
+        if (!in_array('category', explode(',', $param))) {
+            return $resource['category_id'];
+        }
+
+        if ($resource['category'] == null) {
+            return $resource['category_id'];
+        }
+
+        return [
+            'id' => $resource['category']['id'],
+            'title' => $resource['category']->toArray()['categories_translations'][0]['title'],
+            'slug' => $resource['category']['slug']
+        ];
+    }
+
+    /**
+     * Sets keys 'tags' and 'ingredients' if they are set in GET 'with' parameter
+     *
+     * @param Request $request
+     * @param string $key
+     * @param Meals $resource
+     * @return array|bool
+     * if I write type array|mixed next to function, it throws a syntax error because of |
+     */
+    public static function helperWithData(Request $request, string $key, Meals $resource)
+    {
+        if (in_array($key, explode(',', $request->input('with')))) {
+            $dataList = [];
+            foreach ($resource[$key] as $item) {
+                $data['id'] = $item->id;
+                $data['title'] = $item->toArray()[$key . '_translations'][0]['title'];
+                $data['slug'] = $item->slug;
+
+                $dataList[] = $data;
+            }
+            return $dataList;
+        };
+        return false;
+    }
+}
